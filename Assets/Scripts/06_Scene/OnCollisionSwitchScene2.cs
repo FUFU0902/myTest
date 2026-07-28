7ddc77d629c6eeb1a2daf6e0cff23766a623951c
gokuringood <gokuringood@gmail.com>
2026-07-28 19:22:17 +0900

    aa

--- Assets/Scripts/06_Scene/OnCollisionSwitchScene2.cs
+++ Assets/Scripts/06_Scene/OnCollisionSwitchScene2.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class OnCollisionSwitchScene2 : MonoBehaviour
 {
 	//-------------------------------------
