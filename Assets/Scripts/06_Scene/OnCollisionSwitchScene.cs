ee2ec84e509a8fd47a917e6a0fcc7ec6155a98bf
gokuringood <gokuringood@gmail.com>
2026-08-04 18:09:16 +0900

    a

--- Assets/Scripts/06_Scene/OnCollisionSwitchScene.cs
+++ Assets/Scripts/06_Scene/OnCollisionSwitchScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class OnCollisionSwitchScene2 : MonoBehaviour
+public class OnCollisionSwitchScene : MonoBehaviour
 {
 	//-------------------------------------
 	public GameObject targetObject;
