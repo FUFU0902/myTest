d4bd57bf6838151d896de6d1dc31e8dd44c39aa9
gokuringood <gokuringood@gmail.com>
2026-08-17 22:20:21 +0900

    a

---
+++ Assets/Scripts/04_Collision/OnCollisionShow2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections2;
+using System.Collections2.Generic;
+using UnityEngine;
+
+// 衝突すると、表示する
+public class OnCollisionShow2 : MonoBehaviour
+{
+	//-------------------------------------
+	public GameObject targetObject;
+	public string tagName;
+	public GameObject showObject;
+	//-------------------------------------
+
+	void Start()
+	{
+		showObject.SetActive(false);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject == targetObject ||
+			collision.gameObject.tag == tagName)
+		{
+			showObject.SetActive(true); // 表示する
+		}
+	}
+}
