1d637e118cbfb4d9e64cb157d7e2ee91546aafc1
gokuringood <gokuringood@gmail.com>
2026-08-18 18:13:16 +0900

    a

---
+++ Assets/OnKeyPressMove2.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class OnKeyPressMove2 : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private Vector2 moveInput;
+    private Rigidbody2D rbody;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        rbody.gravityScale = 0;
+
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+
+        if (moveInput.x != 0) sr.flipX = moveInput.x < 0;
+    }
+
+    void FixedUpdate()
+    {
+
+        rbody.linearVelocity = moveInput * speed;
+    }
+}
