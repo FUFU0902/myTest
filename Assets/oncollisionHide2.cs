4cc0696f0410e9c882065f8f5484d035df4aca3f
gokuringood <gokuringood@gmail.com>
2026-08-18 17:54:48 +0900

    a

---
+++ Assets/oncollisionHide2.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class OnCollisionHide2 : MonoBehaviour
+{
+    //-------------------------------------
+    public GameObject targetObject;
+    public string tagName;
+    public GameObject hideObject;
+    //-------------------------------------
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject == targetObject ||
+            collision.gameObject.tag == tagName)
+        {
+            hideObject.SetActive(false);
+        }
+    }
+}
